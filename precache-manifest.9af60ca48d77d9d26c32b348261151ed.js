304ca4f932794d5d931007ffea73d26be13c17c3
GabrielGalati <gabrielgalati.js@gmail.com>
2020-07-10 10:45:14 -0400

    Updates

---
+++ precache-manifest.9af60ca48d77d9d26c32b348261151ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c55dd6843e7a1a211637d3c40a7098",
+    "url": "/pp/index.html"
+  },
+  {
+    "revision": "0c4130e7cf254aa6999a",
+    "url": "/pp/static/css/main.c5bad317.chunk.css"
+  },
+  {
+    "revision": "a1cc61d4ed38bca2eee1",
+    "url": "/pp/static/js/2.3c8000bd.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/pp/static/js/2.3c8000bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c4130e7cf254aa6999a",
+    "url": "/pp/static/js/main.39c6c4ff.chunk.js"
+  },
+  {
+    "revision": "37913509240598bec616",
+    "url": "/pp/static/js/runtime-main.d1da4df5.js"
+  },
+  {
+    "revision": "c97a9fc29652bb4afcdac68020e5d0f7",
+    "url": "/pp/static/media/Merriweather-Regular.c97a9fc2.ttf"
+  },
+  {
+    "revision": "52eb73dbce8bf4083a59acc2e91699ad",
+    "url": "/pp/static/media/SpaceMono-Regular.52eb73db.ttf"
+  },
+  {
+    "revision": "e00e2a77dd88a8fe75573a5d993af76a",
+    "url": "/pp/static/media/Ubuntu-Bold.e00e2a77.ttf"
+  }
+]);
